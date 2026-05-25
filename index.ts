e75bcbc93b851f24f758b57afb336da3f6ae5d7c
sparrowsurya <92842745+SparrowSurya@users.noreply.github.com>
2026-05-25 14:08:16 +0530

    updates

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { tool } from "@langchain/core/tools";
 import { SystemMessage, AIMessage, ToolMessage, HumanMessage } from "@langchain/core/messages";
 import {
@@ -10,8 +9,11 @@ import {
   type ConditionalEdgeRouter,
   START,
   END,
+  MemorySaver,
+  type LangGraphRunnableConfig,
 } from "@langchain/langgraph";
 import { z } from "zod";
+import { ChatOllama } from "@langchain/ollama";
 
 
 const add = tool(({ a, b }) => a + b, {
@@ -49,10 +51,16 @@ const toolsByName = {
 type ToolName = keyof typeof toolsByName;
 const tools = Object.values(toolsByName);
 
-const modelWithTools = new ChatGoogleGenerativeAI({
-    model: process.env.MODEL_NAME!,
-    apiKey: process.env.GEMINI_API_KEY!,
-    temperature: 0,
+// const modelWithTools = new ChatGoogleGenerativeAI({
+//     model: process.env.MODEL_NAME!,
+//     apiKey: process.env.GEMINI_API_KEY!,
+//     temperature: 0,
+// }).bindTools(tools);
+
+const modelWithTools = new ChatOllama({
+  model: "gemma4:e2b",
+  baseUrl: "http://127.0.0.1:11434",
+  temperature: 0.7,
 }).bindTools(tools);
 
 const MessagesState = new StateSchema({
@@ -107,18 +115,41 @@ const shouldContinue: ConditionalEdgeRouter<typeof MessagesState, Record<string,
   return END;
 };
 
+const checkpointer = new MemorySaver();
+const config = { configurable: { thread_id: '1' }} as LangGraphRunnableConfig;
+
 const agent = new StateGraph(MessagesState)
   .addNode("llmCall", llmCall)
   .addNode("toolNode", toolNode)
   .addEdge(START, "llmCall")
   .addConditionalEdges("llmCall", shouldContinue, ["toolNode", END])
   .addEdge("toolNode", "llmCall")
-  .compile();
+  .compile({ checkpointer });
 
-const result = await agent.invoke({
-  messages: [new HumanMessage("Add 3 and 4.")],
-});
 
-for (const message of result.messages) {
-  console.log(`[${message.type}]: ${message.text}`);
+console.log("Use '.exit' to quit!");
+let lineNo = 0;
+
+const reader = console[Symbol.asyncIterator]();
+while (true) {
+  process.stdout.write("[HUMAN]: ");
+  const line = ((await reader.next()).value as string).trim();
+  if (line === '.exit') break;
+
+  const messages = [new HumanMessage(line)];
+  const result = await agent.invoke({ messages }, config);
+
+  let lines = 0;
+  for (const msg of result.messages.slice(lineNo)) {
+    if (msg.type === "ai" && msg.text.trim().length > 0) {
+      console.log(`[${msg.type.toUpperCase()}]: ${msg.text}`);
+    }
+    lines += 1;
+  }
+  lineNo += lines;
 }
+
+await reader.return?.();
+
+// const state = await agent.getState(config);
+// console.log("State:", state);
